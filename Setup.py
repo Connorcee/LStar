b282ba197f964f50221a77e60264cc67f725e593
Connor <Taeconnor123@googlemail.com>
2018-01-09 15:44:30 +0000

    create_random_legal_transition added

--- Setup.py
+++ Setup.py
@@ -1,25 +1,28 @@
 from StateMachineComponents import *
 
 if __name__ == '__main__':
+    logging = True
     print 'Generating Machine'
     print '------------------------------------------'
 
     # Create the machine
-    states = 3
+    states = 400
     accepting = 1
-    symbols = [1, 2]
+    symbols = [1,2,3,4,5,6,7,8,9]
     outputs = [0,1]
+    randomise = True
 
-    test_word = [1,2,3]
+    test_word = [1,2,1]
 
     # Create the alphabet object to pass to the state machine
     alphabet = Alphabet(symbols)
     # Create a state machine with the states and symbols
-    Mealy = MealyMachine(states, symbols, outputs)
-    Mealy.random_walk()
-    Mealy.print_states()
-    Mealy.print_machine_transitions()
+    Mealy = MealyMachine(states, symbols, outputs, accepting, randomise)
+    if logging:
+        Mealy.print_states()
+        Mealy.print_machine_transitions()
 
     print 'Generation Complete'
     print '------------------------------------------'
-    print Mealy.is_accepted(test_word)
+    Mealy.is_accepted(test_word,logging)
+    Mealy.create_random_legal_transition()
--- StateMachineComponents.py
+++ StateMachineComponents.py
@@ -2,54 +2,66 @@ from random import *
 
 
 class MealyMachine(object):
-    def __init__(self, number_of_nodes, alphabet, outputs, number_of_accepting=1):
+    def __init__(self, number_of_nodes, alphabet, outputs, number_of_accepting=1, randomise = False):
 
-        self.Number_of_nodes = number_of_nodes
+        self.number_of_nodes = number_of_nodes
         self.alphabet = Alphabet(alphabet)
         self.outputs = Outputs(outputs)
-        self.States = []
-        self.StartingState = None
-        self.States = [State(count) for count in range(0, number_of_nodes)]
+        self.starting_state = None
         self.Walked = False
+        self.states = [State(count) for count in range(0, number_of_nodes)]
+        if randomise:
+            self.random_walk()
 
     def print_states(self):
-        print "States: " + str(self.States)
+        print "States: " + str(self.states)
 
     # Creates a random walk, makes initial state start and final state an acceptor
     def random_walk(self):
         if not self.Walked:
             # Only allow one random walk, doesn't check for duplicate symbol transitions
             self.Walked = True
-            shuffle(self.States)
-            for index, item in enumerate(self.States[:-1]):
+            shuffle(self.states)
+            for index, item in enumerate(self.states[:-1]):
                 # Get the position of a random symbol
                 if index == 0:
                     item.is_start = True
-                    self.StartingState = item
+                    self.starting_state = item
                 random_symbol = choice(self.alphabet.symbols)
                 random_output = choice(self.outputs.outputs)
-                next_state = self.States[index + 1]
+                next_state = self.states[index + 1]
                 item.add_transition(next_state, random_symbol, random_output)
-                if index == len(self.States) - 2:
+                if index == len(self.states) - 2:
                     print "Adding Acceptor"
                     next_state.is_accepting = True
 
+    # Not completely pythonic because states should be a dictionary
+    def create_random_legal_transition(self):
+        legal_states = [x for x in self.states if x.degree < len(self.alphabet)]
+        state = choice(legal_states)
+        state = self.states[self.states.index(state)]
+        transition_state = choice([x for x in self.states if x.id != state.id])
+        legal_symbols = [x for x in self.alphabet.symbols if x not in state.get_transition_symbols()]
+        print "Legal Symbols " + str(legal_symbols)
+        state.add_transition(transition_state,choice(legal_symbols),choice(self.outputs.outputs))
+        state.print_transitions()
+
     def print_machine_transitions(self):
-        for state in self.States:
+        for state in self.states:
             state.print_transitions()
 
     def random_acceptors(self, number_of_acceptors):
-        if number_of_acceptors < len(self.States):
-            temp = sample(self.States, number_of_acceptors)
+        if number_of_acceptors < len(self.states):
+            temp = sample(self.states, number_of_acceptors)
             # Assign all the states in temp to accepting states
             [setattr(x, 'is_accepting', True) for x in temp]
             print "Acceptors: " + str(temp)
 
     def ending_acceptor(self):
-        self.States[len(self.States) - 1].is_accepting = True
+        self.states[len(self.states) - 1].is_accepting = True
 
     def transition_legal(self, state, symbol):
-        if state in self.States:
+        if state in self.states:
             return any(x.symbol == symbol for x in state.Transitions)
 
     def next_state(self, state, symbol):
@@ -62,14 +74,21 @@ class MealyMachine(object):
         else:
             return "No Transition from this state: " + str(State) + " " + str(symbol)
 
-    def is_accepted(self, word):
-        starting_state = [x for x in self.States if x.is_start is True][0]
-        print "Start State:" + str(starting_state)
-
+    def is_accepted(self, word, logging=False):
+        starting_state = [x for x in self.states if x.is_start is True][0]
+        current_state = starting_state
+        for symbols in word:
+            if self.transition_legal(current_state,symbols):
+                if logging: print str(symbols) + " " + "Is legal character, moving state"
+                current_state = self.next_state(current_state,symbols)
+            else:
+                if logging: print "No legal transition from " + str(current_state) + " for the symbol " + str(symbols)
+                return False
+        return True
 
 class State(object):
-    def __init__(self, _id, accepting=False, is_start=False):
-        self._id = _id
+    def __init__(self, id, accepting=False, is_start=False):
+        self.id = id
         self.Transitions = []
         self.is_accepting = accepting
         self.is_start = is_start
@@ -87,18 +106,24 @@ class State(object):
         transition = next(t for t in self.Transitions if t.symbol == symbol)
         return transition
 
+    def get_transition_symbols(self):
+        symbols = []
+        for x in self.Transitions:
+            symbols.append(x.symbol)
+        return symbols
+
     def __getitem__(self, item):
         return self
 
     def __str__(self):
-        return "( ID:" + str(self._id) + " Acceptor:" + str(self.is_accepting) + " Start:" + str(self.is_start) + ")"
+        return "( ID:" + str(self.id) + " Acceptor:" + str(self.is_accepting) + " Start:" + str(self.is_start) + ")"
 
     def __repr__(self):
-        return "( ID:" + str(self._id) + " Acceptor:" + str(self.is_accepting) + " Start:" + str(self.is_start) + ")"
+        return "( ID:" + str(self.id) + " Acceptor:" + str(self.is_accepting) + " Start:" + str(self.is_start) + ")"
 
     # TODO: Might need to be changed to check for a logical equivalence rather than an instance equivalence
     def __eq__(self, other):
-        return self._id == other._id
+        return self.id == other.id
 
 
 class Transition(object):
@@ -140,7 +165,13 @@ class Alphabet(object):
     def __init__(self, symbols):
         self.symbols = list(set(symbols))
 
+    def __len__(self):
+        return len(self.symbols)
+
 # Mealy Machines have outputs on transitions
 class Outputs(object):
     def __init__(self, outputs):
         self.outputs = list(set(outputs))
+
+    def __len__(self):
+        return len(self.outputs)
